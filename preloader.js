f53ed72257e56163349294b8c46c3a52b0df205c
Andr0id16 <pracheth.thamankar@gmail.com>
2022-04-17 06:49:25 +0530

    seperated preloader code from main code

---
+++ preloader.js
@@ -0,0 +1,7 @@
+var socket = document.querySelector(".socket");
+var option = document.getElementById("option");
+
+function preloadershow() {
+  socket.style.display = "block";
+  console.log("loading");
+}
