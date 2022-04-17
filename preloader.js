4cfeea4a87b59087b2d7c38ad8f1c74aa1f34711
Andr0id16 <pracheth.thamankar@gmail.com>
2022-04-17 14:36:01 +0530

    made result calculation asynchronous

--- preloader.js
+++ preloader.js
@@ -1,7 +1,32 @@
 var socket = document.querySelector(".socket");
 var option = document.getElementById("option");
-
+var result = document.querySelector(".result");
+processtime = 8000;
 function preloadershow() {
   socket.style.display = "block";
-  console.log("loading");
+}
+function preloaderhide() {
+  socket.style.display = "none";
+}
+function resultshow() {
+  result.style.display = "block";
+}
+function resulthide() {
+  result.style.display = "none";
+}
+function resultcalculate() {
+  const resultPromise = new Promise((resolve, reject) => {
+    preloadershow();
+    setTimeout(() => {
+      console.log("result calculated");
+      resolve("calculated result");
+    }, processtime);
+  });
+
+  resultPromise.then(putResult);
+}
+
+function putResult() {
+  preloaderhide();
+  resultshow();
 }
