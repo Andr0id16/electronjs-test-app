8695a826bfa1a746604a25a101baa8f6fe8b12ed
pracheth thamankar <pracheth.thamankar@gmail.com>
2022-04-23 20:52:03 +0530

    corrected fetch promise

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var express = require("express");
-var path = require("path");
 const cors = require("cors");
 var app = express();
-var router = express.Router();
 var bodyparser = require("body-parser");
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
@@ -16,11 +14,22 @@ app.post("/", function (req, res) {
     console.log("get");
     var ip_addr = req.body.ip_addr;
     var port = req.body.port;
-    res.write(JSON.stringify({ ip_addr: ip_addr, port: port }));
-    res.end();
+    console.log(ip_addr);
+    res.setHeader("Content-Type", "application/json");
+    res.send(JSON.stringify({ a: 1 }));
   }, 3000);
 });
-
+app.get("/", function (req, res) {
+  setTimeout(() => {
+    // console.log("get");
+    // var ip_addr = req.body.ip_addr;
+    // var port = req.body.port;
+    // console.log(ip_addr);
+    res.setHeader("Content-Type", "text/html");
+    // res.send(JSON.stringify({ a: 1 }));
+    res.send("hello");
+  }, 3000);
+});
 app.listen(3000, () => {
   console.log("listening on port 3000");
 });
